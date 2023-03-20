d41e2d377a5ba067748334df53be03ecbf6b8ffd
Zoey-Vail <122924230+Zoey-Vail@users.noreply.github.com>
2023-03-19 21:33:28 -0400

    Features 1 and 2 Working

--- app.py
+++ app.py
@@ -1,33 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
+
+from src.repositories.movie_repository import get_movie_repository
 
 app = Flask(__name__)
 
-movies = []
+movie_repository = get_movie_repository()
 
-@app.route('/', methods=['GET', 'POST'])
+movie_dict = {}
+key_list = {}
+
+@app.get('/')
 def index():
-    if request.method == 'POST':
-        movie = request.form['movie']
-        rating = request.form['rating']
-        if not rating.isdigit() or int(rating) < 1 or int(rating) > 10:
-            message = 'Rating must be an integer between 1 and 10.'
-        else:
-            movie_rating = (movie, int(rating))
-            if movie_rating not in movies:
-                movies.append(movie_rating)
-                message = f'{movie} has been added to the list of movies with a rating of {rating}.'
-            else:
-                message = f'{movie} with a rating of {rating} is already in the list of movies.'
-        return render_template('index.html', movies=movies, message=message)
-    else:
-        return render_template('index.html', movies=movies)
+    return render_template('index.html')
+
 
 @app.route('/movies')
 def list_all_movies():
-    return render_template('list_all_movies.html', list_movies_active=True, movies=movies)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('list_all_movies.html', list_movies_active=True)
 
 
 @app.get('/movies/new')
@@ -35,14 +24,23 @@ def create_movies_form():
     return render_template('create_movies_form.html', create_rating_active=True)
 
 
-@app.post('/movies')
+@app.route('/movies', methods = ['POST'])
 def create_movie():
     # TODO: Feature 2
     # After creating the movie in the database, we redirect to the list all movies page
-    return redirect('/movies')
+    if request.method == 'POST':
+            title = request.form['title']
+            direc = request.form['director']
+            rate = request.form['rating']
+            key_list = movie_dict.keys()
+            movie_dict.update({title: [direc, rate] })
+    return render_template('list_all_movies.html', key_list = key_list, movie_dict = movie_dict)
 
 
 @app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
     return render_template('search_movies.html', search_active=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
