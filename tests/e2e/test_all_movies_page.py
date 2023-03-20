153a9f8b3827decb78674a5bf3484527af90a53d
ArsalK11 <122924183+ArsalK11@users.noreply.github.com>
2023-03-19 20:26:08 -0400

    Update test_all_movies_page.py

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,9 @@
 # TODO: Feature 1
+from app import app
+
+client = app.test_client()
+
+def test_list_all_movies():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'All Movies' in response.data
